a6ab7435964b6553b8faeba8185eba66cf371f02
dylansawicki15 <dylansawicki15@gmail.com>
2026-06-11 20:13:47 +0200

    chapter 6 parsing expressions

--- com/craftinginterpreters/lox/Expr.java
+++ com/craftinginterpreters/lox/Expr.java
@@ -3,13 +3,17 @@ package com.craftinginterpreters.lox;
 import java.util.List;
 
 abstract class Expr {
+
     interface Visitor<R> {
         R visitBinaryExpr(Binary expr);
         R visitGroupingExpr(Grouping expr);
         R visitLiteralExpr(Literal expr);
         R visitUnaryExpr(Unary expr);
+        R visitTernaryExpr(Ternary expr);
     }
+
     static class Binary extends Expr {
+
         Binary(Expr left, Token operator, Expr right) {
             this.left = left;
             this.operator = operator;
@@ -20,11 +24,14 @@ abstract class Expr {
         <R> R accept(Visitor<R> visitor) {
             return visitor.visitBinaryExpr(this);
         }
+
         final Expr left;
         final Token operator;
         final Expr right;
     }
+
     static class Grouping extends Expr {
+
         Grouping(Expr expression) {
             this.expression = expression;
         }
@@ -33,9 +40,12 @@ abstract class Expr {
         <R> R accept(Visitor<R> visitor) {
             return visitor.visitGroupingExpr(this);
         }
+
         final Expr expression;
     }
+
     static class Literal extends Expr {
+
         Literal(Object value) {
             this.value = value;
         }
@@ -44,9 +54,12 @@ abstract class Expr {
         <R> R accept(Visitor<R> visitor) {
             return visitor.visitLiteralExpr(this);
         }
+
         final Object value;
     }
+
     static class Unary extends Expr {
+
         Unary(Token operator, Expr right) {
             this.operator = operator;
             this.right = right;
@@ -56,8 +69,27 @@ abstract class Expr {
         <R> R accept(Visitor<R> visitor) {
             return visitor.visitUnaryExpr(this);
         }
+
         final Token operator;
         final Expr right;
+    }
+
+    static class Ternary extends Expr {
+
+        Ternary(Expr condition, Expr thenBranch, Expr elseBranch) {
+            this.condition = condition;
+            this.thenBranch = thenBranch;
+            this.elseBranch = elseBranch;
+        }
+
+        @Override
+        <R> R accept(Visitor<R> visitor) {
+            return visitor.visitTernaryExpr(this);
+        }
+
+        final Expr condition;
+        final Expr thenBranch;
+        final Expr elseBranch;
     }
 
     abstract <R> R accept(Visitor<R> visitor);
